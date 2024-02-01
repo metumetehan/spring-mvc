859e42fb09a444581adf0c83511f6bf634f2aac0
metumetehan <metumetehan@gmail.com>
2024-02-01 10:43:03 +0200

    get method
    @GetMapping and student class are implemented

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,14 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hrllo", "Worldd");
-	}
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Metehan",
+						"mete@gmail.com",
+						LocalDate.of(1999, 01, 01),
+						24));
 
+	}
 }
